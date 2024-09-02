e8fed0bb5149b2f1072de53e993c3526c6f56630
veeraj-singh <veerbachche82@gmail.com>
2024-09-02 15:50:29 +0530

    common packages created

---
+++ common/dist/index.d.ts
@@ -0,0 +1,48 @@
+import { z } from 'zod';
+export declare const SignUpInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+    name: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}>;
+export type SignUpType = z.infer<typeof SignUpInput>;
+export declare const SignInInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export type SignInType = z.infer<typeof SignInInput>;
+export declare const CreatePostInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+}, {
+    title: string;
+    content: string;
+}>;
+export type CreatePostType = z.infer<typeof CreatePostInput>;
+export declare const UpdatePostInput: z.ZodObject<{
+    title: z.ZodOptional<z.ZodString>;
+    content: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    title?: string | undefined;
+    content?: string | undefined;
+}, {
+    title?: string | undefined;
+    content?: string | undefined;
+}>;
+export type UpdatePostType = z.infer<typeof UpdatePostInput>;
